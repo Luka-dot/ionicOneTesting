1969cbdd2cc2abe3a3eea83fcc566f47e3f59ec7
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-05-09 20:55:00 -0400

    display result in ion-grid

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const calculateBtn = document.querySelector('ion-button');
 const resetValues = document.getElementById('resetBtn');
 const heightInput = document.getElementById('height');
 const weightInput = document.getElementById('weight');
+const resultArea = document.getElementById('result');
 
 const calculateBmi = () => {
     const Bmi = +weightInput.value / (+heightInput.value * +heightInput.value);
     console.log(Bmi)
+
+    const resultElement = document.createElement('ion-card');
+    resultElement.innerHTML = `
+    <ion-card-content>
+        <h2>Your BMI is: ${Bmi}</h2>
+    </ion-card-context>
+    `;
+    resultArea.innerHTML = '';
+    resultArea.appendChild(resultElement);
 };
 
 const resetBtnClicked = () => {
